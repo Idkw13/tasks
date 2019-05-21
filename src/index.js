2bd2b6ff3e8cfcd609794be6aca622b7c57b6a30
Nazar <nazarmayba@gmail.com>
2019-05-21 17:02:15 +0300

    change folder

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-function iqTest(numbers) {
-    numbers = numbers.split(' ');
+const count = (walk, direction) => (walk.filter(item => item === direction)).length;
 
-    let odd = [];
-    let even = [];
-
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 === 0) {
-            even.push(numbers[i])
-        } else {
-            odd.push(numbers[i])
-        }
-    }
-    let index = 1;
-    if (odd.length > even.length) {
-        index += numbers.indexOf(even[0])
-    } else {
-        index += numbers.indexOf(odd[0])
-    }
-    return index
+function isValidWalk(walk) {
+    return walk.length === 10
+        && count(walk, 's') === count(walk, 'n')
+        && count(walk, 'w') === count(walk, 'e')
 }
 
-let j = iqTest("2 4 7 8 10");
+let j = isValidWalk(['n', '3', 'n', 's', 'n', 's', 'n', 's', 'n', 's']);
 console.log(j);
